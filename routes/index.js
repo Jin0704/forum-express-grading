78cca193ea0b98f3fb46f038498cdce527bb7c2b
Jin <s87160204@gmail.com>
2020-11-24 23:49:43 +0800

    Install passport and passport-local & Add feature:signInPage,signIn and logout

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const restController = require('../controllers/restController')
 const adminComtroller = require('../controllers/adminController')
 const userController = require('../controllers/userController')
-module.exports = app => {
+module.exports = (app, passport) => {
 
   app.get('/', (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', restController.getRestaurants)
@@ -9,6 +9,9 @@ module.exports = app => {
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', adminComtroller.getRestaurants)
   app.get('/signup', userController.signUpPage)
+  app.get('/signin', userController.signInPage)
+  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+  app.get('/logout', userController.logout)
   app.post('/signup', userController.signUp)
 
 }
