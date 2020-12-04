5176026526e2169583ecc45cef1d6ac9a472609c
Jin <s87160204@gmail.com>
2020-12-05 00:19:07 +0800

    Add user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,12 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
+const fs = require('fs')
+const imgur = require('imgur-node-api')
+const { useFakeXMLHttpRequest } = require('sinon')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const User = db.User
+
+const defaultimage = ' https://i.imgur.com/Henzc78.png'
 
 const userController = {
   signUpPage: (req, res) => {
@@ -43,7 +49,54 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        res.render('user', { user: user.toJSON() })
+      })
+  },
+  editUser: (req, res) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        res.render('editUser', { user: user.toJSON() })
+      })
+
+  },
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name field is required")
+      return res.redirect('back')
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        if (err) console.log('Error: ', err)
+        return User.findByPk(req.params.id)
+          .then(user => {
+            user.update({
+              name: req.body.name,
+              image: file ? img.data.link : user.image
+            }).then((user) => {
+              req.flash('success_message', 'User was successfully to update')
+              res.redirect(`/users/${user.id}`)
+            })
+          })
+      })
+    } else {
+      return User.findByPk(req.params.id)
+        .then((user) => {
+          user.update({
+            name: req.body.name,
+            image: user.image
+          }).then((user) => {
+            req.flash('success_messages', 'User was successfully to update')
+            res.redirect(`/users/${user.id}`)
+          })
+        })
+    }
   }
 }
-
 module.exports = userController
