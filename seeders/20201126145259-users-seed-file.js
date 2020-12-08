658de70aa411ceacf235d626cb0fc3951a4bf02f
Jin <s87160204@gmail.com>
2020-12-08 21:10:14 +0800

    Fixing switch user admin/user & Add user default image

--- seeders/20201126145259-users-seed-file.js
+++ seeders/20201126145259-users-seed-file.js
@@ -1,5 +1,6 @@
 'use strict';
 const bcrypt = require('bcryptjs')
+const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -9,21 +10,24 @@ module.exports = {
       isAdmin: true,
       name: 'root',
       createdAt: new Date(),
-      updatedAt: new Date()
+      updatedAt: new Date(),
+      image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
     }, {
       email: 'user1@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user1',
       createdAt: new Date(),
-      updatedAt: new Date()
+      updatedAt: new Date(),
+      image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
     }, {
       email: 'user2@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user2',
       createdAt: new Date(),
-      updatedAt: new Date()
+      updatedAt: new Date(),
+      image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
     }], {})
   },
   down: async (queryInterface, Sequelize) => {
