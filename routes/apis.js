449ed89936bea1cf1a672d40465b6f1cb9130aab
Jin <s87160204@gmail.com>
2020-12-13 17:57:51 +0800

    Add authenticated and authenticatedAdmin

--- routes/apis.js
+++ routes/apis.js
@@ -4,6 +4,18 @@ const router = express.Router()
 const multer = require('multer')
 const { authenticate } = require('passport')
 const upload = multer({ dest: 'temp/' })
+const passport = require('../config/passport')
+
+const authenticated = passport.authenticate('jwt', { session: false })
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) { return next() }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
 
 const adminController = require('../controllers/api/adminControllers')
 const categoryController = require('../controllers/api/categoryController')
@@ -11,16 +23,16 @@ const adminService = require('../services/adminServices')
 const categoryService = require('../services/categoryService')
 const userController = require('../controllers/api/userController')
 
-router.get('/admin/restaurants', adminController.getRestaurants)
-router.get('/admin/restaurants/:id', adminController.getRestaurant)
-router.put('/admin/restaurants/:id', upload.single('image'), adminService.putRestaurant)
-router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
-router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
+router.get('/admin/restaurants', authenticated, authenticatedAdmin, adminController.getRestaurants)
+router.get('/admin/restaurants/:id', authenticated, authenticatedAdmin, adminController.getRestaurant)
+router.put('/admin/restaurants/:id', upload.single('image'), authenticated, authenticatedAdmin, adminService.putRestaurant)
+router.delete('/admin/restaurants/:id', authenticated, authenticatedAdmin, adminController.deleteRestaurant)
+router.post('/admin/restaurants', upload.single('image'), authenticated, authenticatedAdmin, adminController.postRestaurant)
 
-router.get('/admin/categories', categoryController.getCategories)
-router.post('/admin/categories', categoryController.postCategory)
-router.put('/admin/categories/:id', categoryController.putCategory)
-router.delete('admin/categories/:id', categoryController.deleteCategory)
+router.get('/admin/categories', authenticated, authenticatedAdmin, categoryController.getCategories)
+router.post('/admin/categories', authenticated, authenticatedAdmin, categoryController.postCategory)
+router.put('/admin/categories/:id', authenticated, authenticatedAdmin, categoryController.putCategory)
+router.delete('admin/categories/:id', authenticated, authenticatedAdmin, categoryController.deleteCategory)
 
 //JWT signin
 router.post('/signin', userController.signIn)
